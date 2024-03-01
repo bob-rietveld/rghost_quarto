dc72769224538ad67656abbce920a9d108c2ad02
Bob Rietveld <bob.rietveld@new10.com>
2024-03-01 23:33:24 +0100

    Added html cards

--- _extensions/rghost/rghost.lua
+++ _extensions/rghost/rghost.lua
@@ -4,7 +4,7 @@ return {
   end,
 
 ['button'] = function(urls, linkText)
-    return pandoc.RawInline('html', "<div class='kg-card kg-button-card kg-align-center'> <p><a class='kg-btn kg-btn-accent' href="..urls[1]..">Button</a></p></div>")
+    return pandoc.RawInline('html', "<!--kg-card-begin: html--><div class='kg-card kg-button-card kg-align-center'> <p><a class='kg-btn kg-btn-accent' href="..urls[1]..">Button</a></p></div><!--kg-card-end: html-->")
 end
 
 
