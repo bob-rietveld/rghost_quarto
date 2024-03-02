f91149ca21da75486387120f5fba6fb72e896b5f
Bob Rietveld <bob.rietveld@new10.com>
2024-03-02 12:22:45 +0100

    Changed function from RawInline to RawBlock

--- _extensions/rghost/rghost.lua
+++ _extensions/rghost/rghost.lua
@@ -4,7 +4,7 @@ return {
   end,
 
 ['button'] = function(content)
-    return pandoc.RawInline('html', "<!--kg-card-begin: html--><div class='kg-card kg-button-card kg-align-center'><p> <a class='kg-btn kg-btn-accent' href="..content[1]..">"..content[2].."</a></p></div><!--kg-card-end: html-->")
+    return pandoc.RawBlock('html', "<!--kg-card-begin:html--><div class='kg-card kg-button-card kg-align-center'><p><a class='kg-btn kg-btn-accent' href="..content[1]..">"..content[2].."</a></p></div><!--kg-card-end: html-->")
 end
 
 
