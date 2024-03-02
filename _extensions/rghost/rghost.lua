e0cf6f944bf0b78369eea3f926a0357bdf7f0891
Bob Rietveld <bob.rietveld@new10.com>
2024-03-02 16:55:33 +0100

    Added some call out logic

--- _extensions/rghost/rghost.lua
+++ _extensions/rghost/rghost.lua
@@ -5,9 +5,16 @@ return {
 
 ['button'] = function(content)
     return pandoc.RawBlock('html', "<!--kg-card-begin:html--><div class='kg-card kg-button-card kg-align-center'><p><a class='kg-btn kg-btn-accent' href="..content[1]..">"..content[2].."</a></p></div><!--kg-card-end: html-->")
+end ,
+
+["callout"] = function(content)
+
+   -- Check if the text is valid
+ if type(content[1]) == "string" and content[1] ~= "" then html = content[1] else return 'No content' end
+ if not content[2] then emoji = "" else emoji = content[2] end
+
+    return pandoc.RawBlock('html', "<!--kg-card-begin:html--><div class='kg-card kg-callout-card kg-callout-card-accent'><div class='kg-callout-emoji'>"..emoji.."</div><div class='kg-callout-text'>"..html.."</div></div><!--kg-card-end: html-->")
 end
-
-
 
 }
 
